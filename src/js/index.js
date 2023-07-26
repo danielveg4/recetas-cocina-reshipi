b50d6342bf9bbb0d4b6a9d61671d46761a620015
daniel vega <dvgilles@gmail.com>
2023-07-26 14:02:56 +0200

    menu comit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainNavElement = document.getElementById('main-nav');
+
+
+function handleClick(){
+    mainNavElement.classList.toggle('hide');
+    
+}
+
+toggleMenuElement.addEventListener('click', handleClick);
